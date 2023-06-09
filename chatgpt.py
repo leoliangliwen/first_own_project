c02aca3c15da1321f33b9a4f26a2b7bd02374beb
leo <leoliangliwen@gmail.com>
2023-06-08 21:41:03 -0400

    Use api_key in the environment

--- chatgpt.py
+++ chatgpt.py
@@ -1,7 +1,7 @@
 import openai
 import os
 
-openai.api_key  = "sk-"
+openai.api_key = os.environ['OPENAI_API_KEY']
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
